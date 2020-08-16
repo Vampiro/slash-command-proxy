274ffdcdc91858996a9e8fa9707c577e2d054be1
Robert Auld <sayhitorobbie@gmail.com>
2020-08-16 19:07:57 -0400

    Simplified regex grouping in getKeywordMap.

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -9,24 +9,20 @@ export function formulateOutput(outputTemplate: string, prxa, prxr, mm) {
   // "Hello, ((mm.user_name)), your answer is ((prxr.answer))"
   const keywordMap = getKeywordMap(outputTemplate);
 
+  // TODO compare prefix to KeywordPrefixes.. pass attr and appropriate object (prxa, prxr, or mm) to another function that can get the correct value out
+
   return outputTemplate;
 }
 
 export function getKeywordMap(outputTemplate: string): Map<string, string[]> {
   const keywordMap = new Map<string, string[]>();
 
-  // not perfect regex, but will work.. things like prxr.abc[dew]123 will be valid unfortunately.
-  const regex = /\(\(((?:prxa|prxr|mm)\.[a-z0-9\[\]_-]+)\)\)/gi;
+  // creates two groups, (prefix).(attr)
+  const regex = /\(\((prxa|prxr|mm)\.([a-z0-9\[\]_-]+)\)\)/gi;
 
   let matches: string[];
   while ((matches = regex.exec(outputTemplate))) {
-    const keyword = matches[1]; // ex: prxa.abc123
-
-    // group the prefix and the attribute into separate groups
-    const prefixRegexSplit = /((?:prxa|prxr|mm))\.(.*)/g;
-    const subMatch = prefixRegexSplit.exec(keyword);
-    const prefix = subMatch[1];
-    const attr = subMatch[2];
+    const [, prefix, attr] = matches;
 
     if (keywordMap.get(prefix) === undefined) {
       keywordMap.set(prefix, []);
