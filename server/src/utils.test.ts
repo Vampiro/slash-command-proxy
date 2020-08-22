f4118b9dd6eb7047d98ba1ac5335445613919e54
Robert Auld <sayhitorobbie@gmail.com>
2020-08-21 23:45:29 -0400

    Added in method to retrieve keyword value out of a given object. Added ability to debug jest tests.

--- server/src/utils.test.ts
+++ server/src/utils.test.ts
@@ -1,23 +1,48 @@
-import * as Utils from "./utils";
+import {
+  KeywordCollection,
+  formulateOutput,
+  getKeywords,
+  getKeywordValue,
+} from "./utils";
 
-const mm = {};
+const keywordCollection: KeywordCollection = {
+  mm: {
+    channel_id: "abc123",
+    channel_name: "Town Hall",
+    command: "roll",
+    response_url: "http://google.com",
+    team_domain: "aaaaaaaa",
+    team_id: "bbbbbbb",
+    text: "20",
+    token: "de3de3deqd",
+    trigger_id: "ffffff",
+    user_id: "ppppppp",
+    user_name: "raauld",
+  },
+  prx: {},
+  prxa: {},
+  prxr: { result: { abc: ["hello", "world"] } },
+};
 
 test("formulate output", () => {
-  const prxa = {};
-  const prxm = {};
-
-  expect(Utils.formulateOutput("Hello World", prxa, prxm, mm)).toBe(
-    "Hello World"
-  );
+  expect(formulateOutput("Hello World", keywordCollection)).toBe("Hello World");
 });
 
 test("get keyword map from template", () => {
-  const keywordMap = Utils.getKeywordMap(
+  const keywords = getKeywords(
     "Hello ((mm.user_name)). You rolled AAprxr.rAA a ((prxa.20)) and a ((prxa.abc123)) ((prxr.rawr))."
   );
 
-  expect(keywordMap.size).toBe(3);
-  expect(keywordMap.get("prxa")).toEqual(expect.arrayContaining(["abc123"]));
-  expect(keywordMap.get("prxr")).toEqual(expect.arrayContaining(["rawr"]));
-  expect(keywordMap.get("mm")).toEqual(expect.arrayContaining(["user_name"]));
+  expect(keywords.size).toBe(4);
+  expect(keywords.has("prxa.20")).toEqual(true);
+  expect(keywords.has("prxa.abc123")).toEqual(true);
+  expect(keywords.has("prxr.rawr")).toEqual(true);
+  expect(keywords.has("mm.user_name")).toEqual(true);
+});
+
+test("get keyword value", () => {
+  expect(getKeywordValue("mm.user_name", keywordCollection)).toBe("raauld");
+  expect(getKeywordValue("prxr.result.abc[1]", keywordCollection)).toBe(
+    "world"
+  );
 });
--- server/src/utils.ts
+++ server/src/utils.ts
@@ -5,31 +5,85 @@ export enum KeywordPrefix {
   MM = "mm",
 }
 
-export function formulateOutput(outputTemplate: string, prxa, prxr, mm) {
+export type KeywordCollection = {
+  mm: {
+    channel_id: string;
+    channel_name: string;
+    command: string;
+    response_url: string;
+    team_domain: string;
+    team_id: string;
+    text: string;
+    token: string;
+    trigger_id: string;
+    user_id: string;
+    user_name: string;
+  };
+  prx: Object;
+  prxa: Object;
+  prxr: Object;
+};
+
+export function formulateOutput(
+  outputTemplate: string,
+  keywordCollection: KeywordCollection
+) {
   // "Hello, ((mm.user_name)), your answer is ((prxr.answer))"
-  const keywordMap = getKeywordMap(outputTemplate);
+  const keywordsUsed = getKeywords(outputTemplate);
 
   // TODO compare prefix to KeywordPrefixes.. pass attr and appropriate object (prxa, prxr, or mm) to another function that can get the correct value out
+  // const args = keywordMap.get(KeywordPrefix.ARGS);
+  // if (args) {
+  //   for (let i = 0; i < args.length; i++) {}
+  // }
 
   return outputTemplate;
 }
 
-export function getKeywordMap(outputTemplate: string): Map<string, string[]> {
-  const keywordMap = new Map<string, string[]>();
+export function getKeywords(outputTemplate: string): Set<string> {
+  const keywordSet = new Set<string>();
 
   // creates two groups, (prefix).(attr)
-  const regex = /\(\((prxa|prxr|mm)\.([a-z0-9\[\]_-]+)\)\)/gi;
+  const regex = /\(\(([a-z0-9\[\]\._\-]+)\)\)/gi;
 
   let matches: string[];
   while ((matches = regex.exec(outputTemplate))) {
-    const [, prefix, attr] = matches;
-
-    if (keywordMap.get(prefix) === undefined) {
-      keywordMap.set(prefix, []);
-    }
-
-    keywordMap.get(prefix).push(attr);
+    const keyword = matches[1];
+    keywordSet.add(matches[1]); // add the keyword
   }
 
-  return keywordMap;
+  return keywordSet;
+}
+
+// given a keyword string such as prxr.result.abc[0].def, will retrieve the value
+// of this out of the keyword collection
+export function getKeywordValue(keyword: string, kc: KeywordCollection) {
+  const keywordParts = keyword.split(".");
+  let parentObject = kc;
+  for (let i = 0; i < keywordParts.length && parentObject !== undefined; i++) {
+    let val: any;
+    const kp = keywordParts[i];
+
+    // check for array brackets
+    const arrIndexRegex = /\[([0-9]+)\]/;
+    const arrRegexMatch = kp.match(arrIndexRegex);
+    if (arrRegexMatch !== null) {
+      const attr = kp.split("[")[0];
+      const arrIndex = Number(arrRegexMatch[1]);
+      if (
+        parentObject[attr] !== undefined &&
+        parentObject[attr] instanceof Array &&
+        parentObject[attr].length > arrIndex
+      ) {
+        val = parentObject[attr][arrIndex];
+      }
+    } else if (parentObject[kp] !== undefined) {
+      val = parentObject[kp];
+    }
+
+    parentObject = val;
+  }
+
+  // at the end of the loop, parent object becomes the final value, or, if it is not found, undefined
+  return parentObject;
 }
