61e4b1ba07fe1abdffa8cac03d8afe5a99649da7
Robert Auld <sayhitorobbie@gmail.com>
2020-08-27 11:15:35 -0400

    Adding in test for proxy endpoint.

---
+++ server/src/app.test.ts
@@ -0,0 +1,21 @@
+/**
+ * jest env set due to interaction with nock/axios
+ * @jest-environment node
+ */
+
+import * as request from "supertest";
+import * as nock from "nock";
+import app from "./app";
+
+nock("http://roll.diceapi.com")
+  .get("/json/d20")
+  .reply(200, { success: true, dice: [{ value: 9, type: "d20" }] })
+  .persist();
+
+test("proxy endpoint", async () => {
+  const response = await request(app.callback()).get(
+    "/proxy?prx.url=http://roll.diceapi.com/json/d20&prx.output=You rolled a ${res.dice[0].value}!"
+  );
+  expect(response.type).toBe("application/json");
+  expect(response.body.text).toBe("You rolled a 9!");
+});
---
+++ server/src/app.ts
@@ -0,0 +1,59 @@
+import * as Koa from "koa";
+import * as KoaRouter from "@koa/router";
+import * as KoaStatic from "koa-static";
+import * as KoaCors from "@koa/cors";
+import axios from "axios";
+import { KeywordCollection, replaceKeywords } from "./utils";
+
+const app = new Koa();
+app.use(KoaCors());
+const router = new KoaRouter();
+const reactBuildDir = "../client/build/";
+
+type ProxyResponse = {
+  text: string | Object;
+};
+
+router.get("/proxy", async (ctx, next) => {
+  const kc: KeywordCollection = {
+    args: ctx.query.text ? ctx.query.text.split(" ") : [],
+    client: {
+      channel_id: ctx.query.channel_id,
+      channel_name: ctx.query.channel_name,
+      command: ctx.query.command,
+      response_url: ctx.query.response_url,
+      team_domain: ctx.query.team_domain,
+      team_id: ctx.query.team_id,
+      text: ctx.query.text,
+      token: ctx.query.token,
+      trigger_id: ctx.query.trigger_id,
+      user_id: ctx.query.user_id,
+      user_name: ctx.query.user_name,
+    },
+    res: {},
+  };
+
+  let response: ProxyResponse = { text: "" };
+  const destUrl = replaceKeywords(ctx.query["prx.url"], kc);
+  const outputTemplate = ctx.query["prx.output"];
+
+  try {
+    const extResponse = await axios.get(destUrl);
+    kc.res = extResponse.data;
+
+    // if user provided output template, replace keywords
+    // else, just give them back the entire response
+    response.text = outputTemplate
+      ? replaceKeywords(outputTemplate, kc)
+      : kc.res;
+  } catch (error) {
+    response.text = `Encountered error from destination server: ${error}`;
+  }
+
+  ctx.body = response;
+});
+
+app.use(KoaStatic(reactBuildDir));
+app.use(router.middleware());
+
+export default app;
