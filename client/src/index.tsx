ebb8e1d547191d8af94c30babe3a63e77accc1ad
Robert Auld <sayhitorobbie@gmail.com>
2020-08-27 10:59:44 -0400

    Taking out React Strict Mode due to MUI problem with spamming warnings to be fixed in v5.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,14 +4,7 @@ import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-// React.StrictMode disabled due to current blasts of warnings about Material UI Tooltips. To be fixed in MUI v5
-// https://github.com/mui-org/material-ui/issues/13394
-ReactDOM.render(
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
